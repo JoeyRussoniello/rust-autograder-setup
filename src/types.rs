639f316f62e4bf0b16f22fe18a98dd55c0c770f8
Joseph Russoniello <jmrusso@bu.edu>
2025-09-07 10:33:32 -0400

    Implemented markdown_table support for AutoTest struct

--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use markdown_tables::MarkdownTableRow;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AutoTest {
@@ -6,4 +7,17 @@ pub struct AutoTest {
     pub docstring: String,
     pub timeout: u64,
     pub points: u32,
+}
+impl MarkdownTableRow for AutoTest {
+    fn column_names() -> Vec<&'static str> {
+        vec!["Name", "Points", "Description"]
+    }
+
+    fn column_values(&self) -> Vec<String> {
+        vec![
+            self.name.clone(),
+            self.points.to_string(),
+            self.docstring.clone(),
+        ]
+    }
 }
